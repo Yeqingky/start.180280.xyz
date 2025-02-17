6e582d4a64a5491cd8e4292495f0258535f51ff7
YeQingqa <yeqing2002@outlook.com>
2025-02-17 23:30:00 +0800

    上传

---
+++ config.js
@@ -0,0 +1,29 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: '服务状态监控',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'ur2434641-66871b80c93a70542bbd1cc6'
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: 'GitHub',
+      url: 'https://github.com/Yeqingky'
+    },
+    {
+      text: 'Blog',
+      url: 'https://blog.180280.xyz/'
+    },
+  ],
+};
